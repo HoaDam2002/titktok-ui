c569b0c0a08c3f705758776091a2d11f4b53c98b
HoaDam2002 <damvanhoa305202@gmail.com>
2023-04-16 17:09:19 +0700

    fifth commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,5 @@
+import { HeaderOnly } from '~/Components/Layout';
+
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
@@ -8,7 +10,7 @@ const publicRoutes = [
     { path: '/', component: Home },
     { path: '/following', component: Following },
     { path: '/profile', component: Profile },
-    { path: '/upload', component: Upload, layout: null },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
 ];
 
 //router này phải đăng nhập mới vào được
