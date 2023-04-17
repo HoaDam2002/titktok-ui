6a6265e8e1cc5baad9b3fc607a33179712f722f2
HoaDam2002 <damvanhoa305202@gmail.com>
2023-04-17 20:02:59 +0700

    xây dựng ui header (1)

--- src/Components/Layout/components/Header/index.js
+++ src/Components/Layout/components/Header/index.js
@@ -1,5 +1,14 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faCircleXmark,
+    faSpinner,
+    faMagnifyingGlass,
+    faPlus,
+    faEllipsisVertical,
+} from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
 
@@ -7,8 +16,26 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                {/* content
-            logo .... */}
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Tìm kiếm tài khoản và video" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}>
+                    <button className={cx('upload')}>
+                        <FontAwesomeIcon icon={faPlus} /> Tải lên
+                    </button>
+                    <button className={cx('login')}>Đăng nhập</button>
+                    <FontAwesomeIcon className={cx('other')} icon={faEllipsisVertical} />
+                </div>
             </div>
         </header>
     );
