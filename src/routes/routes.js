ae8ebf08645255ef766d54a935c1935f5a71b44d
HoaDam2002 <damvanhoa305202@gmail.com>
2023-05-04 16:29:53 +0700

    tối ưu code

---
+++ src/routes/routes.js
@@ -0,0 +1,22 @@
+import HeaderOnly from '~/layouts/HeaderOnly';
+import config from '~/config';
+
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/layouts/components/Search';
+
+//router này k cần login
+const publicRoutes = [
+    { path: config.routes.home, component: Home },
+    { path: config.routes.following, component: Following },
+    { path: config.routes.profile, component: Profile },
+    { path: config.routes.upload, component: Upload, layout: HeaderOnly },
+    { path: config.routes.search, component: Search, layout: null },
+];
+
+//router này phải đăng nhập mới vào được
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
